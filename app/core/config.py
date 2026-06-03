fd89fe08ca0e966856ef7278dbe8a6192c8bc0a6
AtiqUrRehman17 <atiqu8437@gmail.com>
2026-06-03 05:42:42 -0700

    updatin config

--- app/core/config.py
+++ app/core/config.py
@@ -6,7 +6,7 @@ import os
 class Settings(BaseSettings):
     DATABASE_URL: str = "sqlite:///./tasks.db"
 
-    SECRET_KEY: str = os.getenv("SECRET_KEY", "your_secret_key")
+    SECRET_KEY: str = os.getenv("SECRET_KEY")
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
 
